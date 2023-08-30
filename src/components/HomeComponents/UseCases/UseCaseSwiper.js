7bec9faf2515d05b0dcc27c54fc8a53a2da703b2
ahmar7 <ahmarjabbar7@gmail.com>
2023-08-31 01:59:44 +0500

    update

--- src/components/HomeComponents/UseCases/UseCaseSwiper.js
+++ src/components/HomeComponents/UseCases/UseCaseSwiper.js
@@ -46,12 +46,9 @@ const UseCaseSwiper = () => {
         </a>
         */}
         <div className="Usecases_headTitle__bkrB-">
-          {/* <div
-            className="Typography_font-size-xl__X+CRm mbl-fer Typography_color-white__0vcsJ Typography_weight-700__3PDWn Typography_line-height-120__+PGc9 Usecases_title__UBpIL jss170 use-cases-header"
-            style={{ opacity: 1 }}
-          > 
+          <div className="" style={{ opacity: 1 }}>
             Solutions to Drive Innovation
-          </div>  */}
+          </div>
         </div>
       </div>
       <div>
